811b53b033d774b503c05fdaa7756186ead7b5e8
Aditya Karnam <akarnam37@gmail.com>
2023-04-22 19:11:30 -0500

    chore: add custom exceptions

---
+++ graphql_tools/exceptions.py
@@ -0,0 +1,13 @@
+"""Exceptions class"""
+
+
+class GQLToolsException(Exception):
+    """GQL Tools Exception class"""
+
+    pass
+
+
+class GQLInvalidTypeException(Exception):
+    """GQL Invalid type exception"""
+
+    pass
